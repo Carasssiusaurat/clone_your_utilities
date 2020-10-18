dc3ae1a9b8f1022adee5ac6ab628bcba1f26ff4d
VulkreoN <64506436+VulkreoN@users.noreply.github.com>
2020-10-19 01:51:57 +0200

    on est là hein

--- set_my_repo_2.sh
+++ set_my_repo_2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 read -p "repository name : " repo_name
-read -p "folder nam : " folder_name
+read -p "folder name : " folder_name
 git clone ${repo_name} ${folder_name}
 cp -R stock_my_utilities/* ./${folder_name}
